ef1ab3d6103735743bbe5150b01f8cb173c127a3
wcastand <wcastand@gmail.com>
2022-07-29 14:44:08 +0200

    fix

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { CardForm, StripeProvider } from '@stripe/stripe-react-native'
+import { CardField, useStripe } from '@stripe/stripe-react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<StripeProvider publishableKey="pk_test_oKhSR5nslBRnBZpjO6KuzZeX">
+			<View style={styles.container}>
+				<CardForm
+					postalCodeEnabled={true}
+					placeholders={{
+						number: '4242 4242 4242 4242',
+					}}
+					cardStyle={{
+						backgroundColor: '#FFFFFF',
+						textColor: '#000000',
+					}}
+					style={{
+						width: '90%',
+						height: 500,
+						margin: 30,
+					}}
+					onCardChange={(cardDetails) => {
+						console.log('cardDetails', cardDetails)
+					}}
+					onFocus={(focusedField) => {
+						console.log('focusField', focusedField)
+					}}
+				/>
+			</View>
+		</StripeProvider>
+	)
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
