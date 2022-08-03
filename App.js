3dd5382c7bd43d7bdb005e820676df822898a17f
wcastand <wcastand@gmail.com>
2022-08-03 18:54:18 +0200

    bug expo

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import { useColorScheme } from 'react-native'
 import { CardForm, StripeProvider } from '@stripe/stripe-react-native'
-import { CardField, useStripe } from '@stripe/stripe-react-native'
+import { Platform } from 'react-native-web'
 
 export default function App() {
+	const color = useColorScheme()
+	console.log(Platform.OS, color)
 	return (
 		<StripeProvider publishableKey="pk_test_oKhSR5nslBRnBZpjO6KuzZeX">
 			<View style={styles.container}>
 				<CardForm
-					style={{
-						width: '90%',
-						height: 500,
-						margin: 30,
-					}}
-					onCardChange={(cardDetails) => {
-						console.log('cardDetails', cardDetails)
-					}}
+					style={{ width: '90%', height: 500, margin: 30 }}
+					cardStyle={{ backgroundColor: color === 'dark' ? 'white' : 'white' }}
+					onCardChange={(cardDetails) => console.log('cardDetails', cardDetails)}
 				/>
 			</View>
 		</StripeProvider>
